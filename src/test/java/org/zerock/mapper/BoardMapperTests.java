30137b3e2c234283e6b2a99524b1d90a81aa3d2b
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-10-18 13:01:47 +0900

    BoardMapperTests 클래스는 스프링을 이용해서 BoardMapper 인터페이스의 구현체를 주입받아 동작하게 함

--- src/test/java/org/zerock/mapper/BoardMapperTests.java
+++ src/test/java/org/zerock/mapper/BoardMapperTests.java
@@ -15,7 +15,7 @@ import lombok.extern.log4j.Log4j;
 public class BoardMapperTests {
 
 	@Setter(onMethod_= @Autowired)
-	private BoardMapper mapper;
+	private BoardMapper mapper; //BoardMapperTests 클래스는 스프링을 이용해서 BoardMapper 인터페이스의 구현체를 주입받아 동작하게 함 
 	
 	@Test
 	public void testGetList() {
